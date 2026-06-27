92bc05577c9bf5ec14da0da2cb1511894396fcd4
RODRIGO TAVARES FERREIRA <git@tavaresferreira.net>
2026-06-27 17:41:31 -0300

    refactor: trata erros do check-in no CLI e limpa o composition root
    Envolve a execucao do caso de uso em um try/catch que reporta as excecoes de dominio e de adapter como mensagem amigavel com exit 1, em vez de vazar stack trace. Extrai a montagem e o disparo do check-in para a funcao realizarCheckin(), deixando o try/catch com uma unica acao (tratamento de erro como responsabilidade unica). Unifica o estilo de controle de fluxo com chaves e remove o import comentado (codigo morto).

--- src/index.php
+++ src/index.php
@@ -2,23 +2,31 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-//use Tavares\Hotel\Domain\VO\Hospede;
 use Tavares\Hotel\Adapter\JSON\QuartoRepositoryJSON;
 use Tavares\Hotel\Adapter\JSON\ReservaRepositoryJSON;
 use Tavares\Hotel\Adapter\NotificacaoEmTela;
-
 use Tavares\Hotel\Application\ClienteFazCheckIN;
 
-if ($argc < 3):
+function realizarCheckin(string $idArg, string $dataArg): void
+{
+    $idReserva   = (int) $idArg;
+    $dataCheckin = new DateTime($dataArg);
+
+    $quartoRepository  = new QuartoRepositoryJSON();
+    $reservaRepository = new ReservaRepositoryJSON($quartoRepository);
+
+    $useCase = new ClienteFazCheckIN($reservaRepository, $quartoRepository, new NotificacaoEmTela());
+    $useCase->acionar($idReserva, $dataCheckin);
+}
+
+if ($argc < 3) {
     echo "Uso: php src/index.php <idReserva> <dataCheckin no formato Y-m-d>\n";
     exit(1);
-endif;
+}
 
-$idReserva   = (int) $argv[1];
-$dataCheckin = new DateTime($argv[2]);
-
-$quartoRepository = new QuartoRepositoryJSON();
-$reservaRepository = new ReservaRepositoryJSON($quartoRepository);
-
-$useCase = new ClienteFazCheckIN($reservaRepository,$quartoRepository,new NotificacaoEmTela());
-$useCase->acionar($idReserva, $dataCheckin);
+try {
+    realizarCheckin($argv[1], $argv[2]);
+} catch (Throwable $e) {
+    echo "Erro: " . $e->getMessage() . "\n";
+    exit(1);
+}
