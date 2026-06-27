bdb062de288357c36eea2ac40552f53e7b7457b6
RODRIGO TAVARES FERREIRA <git@tavaresferreira.net>
2026-06-27 16:44:12 -0300

    feat(adapter): implementa repositorio de reservas em JSON
    Implementa ReservaRepositoryJSON, adaptador da porta ReservaRepository que persiste reservas em data/reservas.json. O construtor valida a existencia do arquivo (ArquivoNaoEncontradoException) e o carrega. O buscar() reconstroi a entidade Reserva resolvendo o hospede (a partir do cpfHospede) e o quarto (delegando ao QuartoRepositoryJSON pelo idQuarto), lancando ReservaNaoEncontradaException quando o id nao existe. O salvar() atualiza a reserva no arquivo, gravando as datas como string no formato Y-m-d para preservar o schema do JSON.

---
+++ src/Adapter/JSON/ReservaRepositoryJSON.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Tavares\Hotel\Adapter\JSON;
+
+use DateTime;
+use Tavares\Hotel\Domain\Reserva;
+use Tavares\Hotel\Domain\VO\Hospede;
+use Tavares\Hotel\Port\ReservaRepository;
+use Tavares\Hotel\Adapter\JSON\QuartoRepositoryJSON;
+use Tavares\Hotel\Adapter\Exception\ArquivoNaoEncontradoException;
+use Tavares\Hotel\Adapter\Exception\ReservaNaoEncontradaException;
+
+class ReservaRepositoryJSON implements ReservaRepository
+{
+    private string $filepath;
+    private array $reservas;
+    private QuartoRepositoryJSON $quartoRepository;
+
+    public function __construct(){
+        $this->filepath = __DIR__ . "/../../../data/reservas.json";
+
+        if(!file_exists($this->filepath)):
+            throw new ArquivoNaoEncontradoException($this->filepath);
+        endif;
+
+        $this->reservas = json_decode( file_get_contents($this->filepath) ,true);
+        $this->quartoRepository = new QuartoRepositoryJSON();
+    }
+
+    public function buscar(int $id):Reserva
+    {
+        
+        foreach($this->reservas as $reserva):
+            if($reserva['id'] == $id):
+               
+                return new Reserva(
+                   $reserva['id'],
+                   new Hospede($reserva['cpfHospede']),
+                   $this->quartoRepository->buscar($reserva['idQuarto']),
+                   new DateTime($reserva['entrada']),
+                   new DateTime($reserva['saida']),
+                   $reserva['utilizada']
+                );
+
+            endif;
+        endforeach;
+
+        throw new ReservaNaoEncontradaException($id);
+    }
+    
+    public function salvar(Reserva $reserva):void
+    {
+        $localizou = false;
+        for($c = 0; $c < count($this->reservas) && $localizou == false ; $c++)
+        {
+            if($this->reservas[$c]['id'] == $reserva->id()):
+                $this->reservas[$c] = [
+                    'id'     => $reserva->id(),
+                    'cpfHospede' => $reserva->hospede()->getCPF(),
+                    'idQuarto' => $reserva->quarto()->id(),
+                    'entrada' => $reserva->getEntrada()->format('Y-m-d'),
+                    'saida' => $reserva->getSaida()->format('Y-m-d'),
+                    'utilizada' => $reserva->isUtilizada()
+                ];
+                $localizou = true;
+            endif;
+        }
+
+        if($localizou == false):
+            throw new ReservaNaoEncontradaException($reserva->id());
+        endif;
+
+        file_put_contents( $this->filepath, json_encode($this->reservas,JSON_PRETTY_PRINT) );
+    }
+}
