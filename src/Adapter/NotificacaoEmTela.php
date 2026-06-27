4f920e556285a6af4a748a9c31f900994b8f5f2b
RODRIGO TAVARES FERREIRA <git@tavaresferreira.net>
2026-06-27 16:52:06 -0300

    feat(adapter): adiciona adaptador de notificacao em tela
    Implementa NotificacaoEmTela, adaptador concreto da porta NotificacaoRepository destinado a entregar ao cliente, em tela, a mensagem de confirmacao do check-in.

---
+++ src/Adapter/NotificacaoEmTela.php
@@ -0,0 +1,13 @@
+<?php
+namespace Tavares\Hotel\Adapter;
+
+use Tavares\Hotel\Port\NotificacaoRepository;
+
+
+class NotificacaoEmTela implements NotificacaoRepository{
+    
+    public function notificarCliente(string $mensagem):void
+    {
+
+    }
+}
